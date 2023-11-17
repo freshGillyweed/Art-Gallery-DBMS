ae39401003de08a889f2c2586176388399577d4e
UBC Student <natarias@students.cs.ubc.ca>
2023-11-16 17:09:10 -0800

    Init

---
+++ src/controller/Controller.java
@@ -0,0 +1,48 @@
+/**
+ * This is the main controller class that will orchestrate everything.
+ * This is based off the Sample Java Project (Bank.java)
+ */
+public class Controller {
+    private DatabaseConnectionHandler dbHandler = null;
+    private LoginWindow loginWindow = null;
+
+    public Controller() {
+        dbHandler = new DatabaseConnectionHandler();
+    }
+
+    private void start() {
+        loginWindow = new LoginWindow();
+        loginWindow.showFrame(this);
+    }
+
+    /**
+     * LoginWindowDelegate Implementation
+     *
+     * connects to Oracle database with supplied username and password
+     */
+    public void login(String username, String password) {
+        boolean didConnect = dbHandler.login(username, password);
+
+        if (didConnect) {
+            // Once connected, remove login window and start text transaction flow
+            loginWindow.dispose();
+
+            TerminalTransactions transaction = new TerminalTransactions();
+            transaction.setupDatabase(this);
+            transaction.showMainMenu(this);
+        } else {
+            loginWindow.handleLoginFailed();
+
+            if (loginWindow.hasReachedMaxLoginAttempts()) {
+                loginWindow.dispose();
+                System.out.println("You have exceeded your number of allowed attempts");
+                System.exit(-1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.start();
+    }
+}
---
+++ src/model/EventModel.java
@@ -0,0 +1,40 @@
+
+/**
+ *  The intent for this class is to update/store information about an Event
+ */
+public class EventModel {
+
+    private int ticketSold;
+    private final String location;
+    private final String date;
+    private final int capacity;
+    private final int eventID;
+
+    public EventModel(int eventID, String date, String location, int ticketSold, int capacity) {
+        this.eventID = eventID;
+        this.date = date;
+        this.location = location;
+        this.ticketSold = ticketSold;
+        this.capacity = capacity;
+    }
+
+    // GETTER METHODS
+    public int getTicketSold() {
+        return ticketSold;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+    public int getEventID() {
+        return eventID;
+    }
+}
