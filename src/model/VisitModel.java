b075b3ade68495338db27721c4160fc77a250ac7
UBC Student <natarias@students.cs.ubc.ca>
2023-11-27 14:10:35 -0800

    implement VisitModel

--- src/model/VisitModel.java
+++ src/model/VisitModel.java
@@ -1,4 +1,19 @@
 package model;
 
 public class VisitModel {
+    private int visitorID;
+    private int exhibitionID;
+
+    public VisitModel(int visitorID, int exhibitionID) {
+        this.visitorID = visitorID;
+        this.exhibitionID = exhibitionID;
+    }
+
+    public int getVisitorID() {
+        return visitorID;
+    }
+
+    public int getExhibitionID() {
+        return exhibitionID;
+    }
 }
