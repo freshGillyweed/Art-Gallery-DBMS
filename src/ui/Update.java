92425d5a7c6f3b6ccb24c6f289272ce5e065b576
Tyler Kerswell <tylerkerswell@gmail.com>
2023-11-29 21:06:38 -0800

    implemented the update box

--- src/ui/Update.java
+++ src/ui/Update.java
@@ -8,19 +8,52 @@ public class Update extends JFrame {
     Update(MainWindowDelegate del) {
         super("Update an artwork");
         delegate = del;
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 400);
+        setLocationRelativeTo(null);
+    }
 
-        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+    public void showUpdateWindow() {
 
-        JLabel prompt = new JLabel("Select an artwork to update:");
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
-        JComboBox<String> artworks = new JComboBox<String>();
+        JLabel prompt = new JLabel("Select the ID of an artwork you want to update:");
 
+
+        JComboBox<String> artworks = new JComboBox<String>(delegate.getArtworkTitles());
+
+        JLabel prompt1 = new JLabel("Attribute to change:");
+
+        JComboBox<String> attr = new JComboBox<String>(new String[]{"artistID", "title", "dimensions", "dateCreated",
+                "value", "donorID", "featureID", "displayMedium"});
+
+        JLabel prompt2 = new JLabel("Change to:");
         JTextField entry = new JTextField();
+        entry.setSize(200, 100);
 
         JButton submit = new JButton("submit");
         submit.addActionListener(e -> {
-
+            try {
+                delegate.update(artworks.getSelectedItem().toString(),
+                        attr.getSelectedItem().toString(), entry.getText().toString());
+                submit.setText("Success!");
+                String[] newTitles = delegate.getArtworkTitles();
+                artworks.setModel(new DefaultComboBoxModel(newTitles));
+            } catch (Exception k) {
+                submit.setText("Error! Could not update entry.");
+            }
         });
 
+        panel.add(prompt);
+        panel.add(artworks);
+        panel.add(prompt1);
+        panel.add(attr);
+        panel.add(prompt2);
+        panel.add(entry);
+        panel.add(submit);
+
+        add(panel);
+        setVisible(true);
     }
 }
